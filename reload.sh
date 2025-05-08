6f4db2cecee0033074be5526f16f0e65bdb1dc03
masterofsql <travellisima@gmail.com>
2025-05-08 15:55:27 +0100

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -1,3 +1,12 @@
+#
+# Commands to run to get this file:
+# git clone https://github.com/masterofsql/QA-DA4-M7.git qa-labs
+# cp qa-labs/reload.sh .
+# dos2unix reload.sh
+# chmod +x reload.sh
+# ./reload.sh
+#
+#
 cd ~
 rm -rf qa-labs
 git clone https://github.com/masterofsql/QA-DA4-M7.git qa-labs
